a6cd6d7ba43f6f61ee52c348cb7cf0dda307dd2f
peterfoo22 <psuh22@gmail.com>
2019-05-20 03:44:56 +1000

    Config File Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
 
-
-get 'pages/home', to: 'pages#home'
+root 'pages#home'
 
 get 'pages/about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
