59617891a14a93d54ade5b09599de7676727fade
Peter Suh <petersuh@Peters-MacBook-Pro.local>
2019-06-02 12:58:57 -0700

    create new template, action, route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ root 'pages#home'
 
 get 'pages/about', to: 'pages#about'
 
+resources :articles
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
